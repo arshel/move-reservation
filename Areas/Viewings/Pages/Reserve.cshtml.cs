225535e63652420f8c7d0b5ee2aea08b893d4008
Laytan <laytanlaats@hotmail.com>
2021-05-22 20:02:19 +0200

    fix bug where database is in use when viewings.Movie getter tries to access it

--- Areas/Viewings/Pages/Reserve.cshtml.cs
+++ Areas/Viewings/Pages/Reserve.cshtml.cs
@@ -45,24 +45,30 @@ namespace movie_reservation.Areas.Viewings.Pages {
             var viewingId = Int32.Parse(Request.Form["viewingId"].ToString());
             var seats = Request.Form["seats"].ToArray();
 
+            List<string> takenSeats;
+            Viewing viewing;
+
             using(var db = new LiteDatabase(@"movieReservation.db")) {
                 var col = db.GetCollection<Models.Viewing>("viewings");
-                var Reservcol = db.GetCollection<Models.Reservation>("Reservations");
-                var viewing = col.FindOne(x => x.Id == viewingId);
-                var takenSeats = seats.Concat(viewing.SeatsTaken).ToList();
+                viewing = col.FindOne(x => x.Id == viewingId);
+                takenSeats = seats.Concat(viewing.SeatsTaken).ToList();
                 viewing.SeatsTaken = takenSeats;
-                
-                col.Update(viewing);
 
-                Reservcol.Insert(new Models.Reservation {
-                    Name = _auth.User.Email,
-                    Seats = takenSeats,
-                    Time = viewing.Time,
-                    Price = viewing.Movie.Price,
-                    UserId = _auth.User.Id,
-                    ReservationTime = DateTime.Now.Date,
-                });
-                
+                col.Update(viewing);
+            }
+
+            var reservation = new Models.Reservation {
+                Name = _auth.User.Email,
+                Seats = takenSeats,
+                Time = viewing.Time,
+                Price = viewing.Movie.Price,
+                UserId = _auth.User.Id,
+                ReservationTime = DateTime.Now.Date,
+            };
+
+            using(var db = new LiteDatabase(@"movieReservation.db")) {
+                var col = db.GetCollection<Models.Reservation>("reservations");
+                col.Insert(reservation);
             }
 
             return RedirectToPage("Index");
